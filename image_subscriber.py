cfa1c91ad885c9d8b00b64fe6bc67163f04cc7f3
Pavan Poladi <pavanpoladi@tamu.edu>
2022-10-17 19:54:14 -0500

    Converted ROS img to CV2 to PIL to tensor image

--- image_subscriber.py
+++ image_subscriber.py
@@ -2,25 +2,45 @@
 import sys
 import cv2
 import rospy
+import torch
 import numpy as np
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from PIL import Image as displayImage
+import torchvision.transforms as transforms
+
 
 bridge = CvBridge()
+
+
 def callback_Img(data):
     img = bridge.imgmsg_to_cv2(data, desired_encoding='rgb8')
+    
+    #convert ROS img_msg to cv2 format
+    img = bridge.imgmsg_to_cv2(data, desired_encoding='rgb8')
+
+    #convert colors to cv2 format
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    #convert to PIL format
+    pilImg = Image.fromarray(img)
+
+    #Convert PIL to Tensor format
+    trans = transforms.Compose([transforms.PILToTensor()])
+    tensorImage = trans(pilImg)
+    
     # Start coordinate, here (5, 5)
-# represents the top left corner of rectangle
+    # represents the top left corner of rectangle
     start_point = (5, 5)
   
-# Ending coordinate, here (220, 220)
-# represents the bottom right corner of rectangle
+    # Ending coordinate, here (220, 220)
+    # represents the bottom right corner of rectangle
     end_point = (220, 220)
   
-# Blue color in BGR
+    # Blue color in BGR
     color = (255, 0, 0)
   
-# Line thickness of 2 px
+    #Line thickness of 2 px
     thickness = 2
     img = cv2.rectangle(img, start_point, end_point, color, thickness)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -29,7 +49,8 @@ def callback_Img(data):
     grayImageMsg.encoding = '8UC1'
     grayImgPub.publish(grayImageMsg)
 
+
 rospy.init_node('img_record_node')
 rospy.Subscriber("/front_camera/image_raw", Image, callback_Img)
 grayImgPub = rospy.Publisher('/img_gray', Image, queue_size=10)
-rospy.spin()
+rospy.spin() #Allows Ros to rotate through images like a loop
