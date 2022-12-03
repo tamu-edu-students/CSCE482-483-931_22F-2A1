90b24cceb49017dbe845de3456e42df8d1576077
Pavan Poladi <ppoladi@gmail.com>
2022-12-02 18:31:12 -0600

    Added Scatterplot for HN Precision Data

--- testing/resultsAggregationHN.py
+++ testing/resultsAggregationHN.py
@@ -13,3 +13,11 @@ print("Precision Data Frame:\n", precisionsDF)
 print("\nNumber of Precision Samples Taken:", len(precisionsDF))
 
 print("\nMean", precisionsDF.mean())
+
+fig, axes = plt.subplots(1, 1)
+fig.suptitle('Precision Scatterplot')
+axes.set_xlabel("Data Sample")
+axes.set_ylabel("Precision")
+sns.scatterplot(data=precisionsDF, x=precisionsDF.index//7, y="Precision")
+fig.set_size_inches(10, 10)
+plt.show()
